b2ad85c4ba47e68fe99a1c48cd340563df004195
Tsolayemi Etchie <favouretchie07@gmail.com>
2026-09-19 15:45:32 +0100

    completed dictionaries python

--- maps/dictionaries.py
+++ maps/dictionaries.py
@@ -44,9 +44,13 @@ import datetime
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-ageInDays = None
-ageInMonths = None
-ageInYears = 
+dateofBirth = datetime.datetime(getMyAge["DOB"]["year"], getMyAge["DOB"]["month"], getMyAge["DOB"]["day"])
+currentDate = datetime.datetime.now()
+delta = currentDate - dateofBirth
+
+ageInDays = delta.days
+ageInMonths = ageInDays / 12
+ageInYears = ageInDays / 365
 
 ########################################################################
 
@@ -79,7 +83,10 @@ import copy
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-deepDictionaryCopy = None
+deepDictionaryCopy = copy.deepcopy(deepDictionary)
+deepDictionaryCopy["name"] = "Kravis Lott"
+deepDictionaryCopy["siblings"].pop()
+
 
 ########################################################################
 
@@ -114,6 +121,17 @@ student = {
 studentCopy = dict(student)
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
+
+student["grade"] += 1
+student["gpa"] -= 0.15
+
+newCourse = {
+	"subject": "MATH",
+	"level": 204,
+}
+student["courses"].append(newCourse)
+
+student["absences"] = 1
 
 
 
